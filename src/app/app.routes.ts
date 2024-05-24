312f47864ac2ee0459ae6a90ad675ed3266ae47a
Takeitiz <tuandat8103@gmail.com>
2024-05-25 00:21:25 +0700

    feat: create sign-in and sign-up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './login/sign-in/sign-in.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth', component: LoginComponent,
+        children: [
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: SignInComponent },
+            { path: 'signup', component: SignUpComponent }
+        ]
+    }
+];
