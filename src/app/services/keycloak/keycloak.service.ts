7f49f6afe31fa14dbc54f9c09cb7ad82d3696930
Takeitiz <tuandat8103@gmail.com>
2024-05-25 14:54:19 +0700

    feat: keycloak integration with angular

---
+++ src/app/services/keycloak/keycloak.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import Keycloak from 'keycloak-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeycloakService {
+
+  private _keycloak: Keycloak | undefined;
+
+  get keycloak() {
+    if (!this._keycloak) {
+      this._keycloak = new Keycloak({
+        url: 'http://localhost:9090',
+        realm: 'social-network',
+        clientId: 'bsn'
+      })
+    }
+    return this._keycloak;
+  }
+
+  constructor() { }
+
+  async init() {
+    console.log("Authentication the user...");
+    const authenticated = await this.keycloak?.init({
+      onLoad: 'login-required'
+    });
+
+    if (authenticated) {
+
+    }
+  }
+}
